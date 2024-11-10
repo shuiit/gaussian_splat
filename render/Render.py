5c33ca952200f220ac09dcab7a010af601675672
roni-maya <roni.maya@mail.huji.ac.il>
2024-11-10 08:39:33 +0200

    2dGS

--- render/Render.py
+++ render/Render.py
@@ -13,8 +13,8 @@ class Render():
             image_size (list): Dimensions of the output image [height, width].
         """
         pixels = cam.project_with_proj_mat(gs.xyz)
-        points_camera = cam.rotate_world_to_cam(cam.homogenize_coordinate(gs.xyz))
-        idx_by_depth = np.argsort(points_camera[:,2])
+        self.points_camera = cam.rotate_world_to_cam(cam.homogenize_coordinate(gs.xyz))
+        idx_by_depth = np.argsort(self.points_camera[:,2])
         gs = gs
         gs.rearange_gs(idx_by_depth)
         projected_pixels=pixels[idx_by_depth]
@@ -26,6 +26,7 @@ class Render():
         tile_coords_range = [(x_idx,y_idx) for x_idx in range(tiles[0], tiles[1]) for y_idx in range(tiles[0], tiles[1])]
         self.tiles = {(x_idx,y_idx): self.get_current_tile_params(gs,x_idx,y_idx,projected_pixels) for x_idx,y_idx in tile_coords_range}
         self.rendered_image = np.ones((image_size[0],image_size[1],3))
+        self.depth = np.ones((image_size[0],image_size[1],3))
         
 
     def calc_pixel_value(self,tile_params,pixel):
@@ -40,12 +41,16 @@ class Render():
         Returns:
             tuple: Color value for the pixel and remaining transparency.
         """
-        d = tile_params['projection']   - pixel 
+        
+        d = tile_params['projection'][:,0:2]   - pixel 
         # power is the gaussian distirbuition. we get the amplitude of each gaussian that impact this pixel. 
         power = -0.5 * (tile_params['conic'][:,0] * d[:,0] * d[:,0] + tile_params['conic'][:,2] * d[:,1] * d[:,1]) - tile_params['conic'][:,1] * d[:,0] * d[:,1]
         alpha = np.minimum(0.99,  tile_params['conic'][:,3]*np.exp(power))
         idx_to_keep = (alpha>1/255) & (power <= 0)
-        return self.sum_all_gs_in_tile(alpha[idx_to_keep],tile_params['color'][idx_to_keep])
+        image,T = self.sum_all_gs_in_tile(alpha[idx_to_keep],tile_params['color'][idx_to_keep])
+        depth,T = self.sum_all_gs_in_tile(alpha[idx_to_keep],tile_params['cam_coord'][idx_to_keep,2])
+
+        return image,T,np.array(depth)
 
     def get_pixels_in_tile(self,pix_start_end):
         """
@@ -82,8 +87,9 @@ class Render():
         pixels_in_tile = self.get_pixels_in_tile(pix_start_end)
         if len(self.tiles[tile]['projection']) > 0:
             for pixel in pixels_in_tile:
-                pixel_value,T = self.calc_pixel_value(self.tiles[tile],pixel)
+                pixel_value,T,depth = self.calc_pixel_value(self.tiles[tile],pixel)
                 self.rendered_image[pixel[1],pixel[0]] = pixel_value + T*np.array([1,1,1])
+                self.depth[pixel[1],pixel[0]] = depth 
 
 
 
@@ -104,7 +110,7 @@ class Render():
         count_within_bounds = np.where(np.sum((self.bounding_box[0] <= [x_idx,y_idx]  ) & (self.bounding_box[1] >= [x_idx,y_idx] ), axis = 1) == 2)[0]
         tile_params = {'xyz': gs.xyz[count_within_bounds], 'conic':gs.conic[count_within_bounds],
                     'color': gs.color[count_within_bounds], 'opacity' : gs.opacity[count_within_bounds],
-                    'projection': projected_pixels[count_within_bounds,0:2]}
+                    'projection': projected_pixels[count_within_bounds,0:3],'cam_coord': self.points_camera[count_within_bounds]}
         return tile_params
 
     def sum_all_gs_in_tile(self,alpha,color): 
